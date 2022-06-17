006db8b91ec072c68d9a97e5865226bb3d30e1a4
felixyh <felix_yanghui@sina.cn>
2022-06-17 11:50:43 +0800

    update readme

---
+++ Boto3/test_01.py
@@ -0,0 +1,33 @@
+import boto3
+
+ec2_client = boto3.client('ec2')
+response = ec2_client.describe_instances(
+    # Filters=[
+    #     {
+    #         # 'Name': 'string',
+    #         # 'Values': [
+    #         #     'string',
+    #         # ],
+    #         # 'Name': 'availability-zone',
+    #         # 'Values': [
+    #         #     'us-east-2',
+    #         #     'us-east-1',
+    #         #     'us-west-1',
+    #         #     'us-west-2',
+    #         ]
+    #     },
+    # ],
+    # # InstanceIds=[
+    # #     'string',
+    # # ],
+    # DryRun=False,
+    # MaxResults=123,
+    # # NextToken='string'
+)
+
+for r in response['Reservations']:
+    for i in r['Instances']:
+        print(i['InstanceId'], i['Hypervisor'])
+        for b in i['BlockDeviceMappings']:
+            print(b['Ebs']['DeleteOnTermination'])
+
